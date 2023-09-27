a01000745d650e91b8a0f8c8980593ef6dd2848d
Brandon Ferrari <28712132+ferrarib@users.noreply.github.com>
2023-09-26 20:23:29 -0400

    added UI to rock paper scissors game

--- index.js
+++ index.js
@@ -8,86 +8,125 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == 'rock') {
         if (computerSelection.toLowerCase() == 'rock') {
-            alert("It's a tie! Rock cancels out Rock");
-            return 0;
+            return ["It's a tie! Rock cancels out Rock", 0];
         }
         else if (computerSelection.toLowerCase() == 'scissors') {
-            alert("You win! Rock crushes Scissors");
-            return 1;
+            return ["You win! Rock crushes Scissors", 1];
         }
         else {
-            alert("You lose! Paper covers Rock");
-            return 2;
+            return ["You lose! Paper covers Rock", 2];
         }
     }
     else if (playerSelection.toLowerCase() == 'scissors') {
         if (computerSelection.toLowerCase() == 'rock') {
-            alert("You lose! Rock crushes Scissors");
-            return 2;
+            return ["You lose! Rock crushes Scissors", 2];
         }
         else if (computerSelection.toLowerCase() == 'scissors') {
-            alert("It's a tie! Scissors cancels out Scissors");
-            return 0;
+            return ["It's a tie! Scissors cancels out Scissors", 0];
         }
         else {
-            alert("You Win! Scissors cuts Paper");
-            return 1;
+            return ["You Win! Scissors cuts Paper", 1];
         }
     }
     else if (playerSelection.toLowerCase() == 'paper') {
         if (computerSelection.toLowerCase() == 'rock') {
-            alert("You win! Paper covers Rock");
-            return 1;
+            return ["You win! Paper covers Rock", 1];
         }
         else if (computerSelection.toLowerCase() == 'scissors') {
-            alert("You lose! Scissors cuts Paper");
-            return 2;
+            return ["You lose! Scissors cuts Paper", 2];
         }
         else{
-            alert("It's a tie! Paper cancels out Paper");
-            return 0;
+            return ["It's a tie! Paper cancels out Paper", 0];
         }
     }
     else {
-        alert("Invalid Option! Try Again!");
-        return -1;
+        return ["Invalid Option! Try Again!", -1];
     }
 }
 
-function game() {
+function ProcessRound(playerChoice) {
+    const [msg, res] =  playRound(playerChoice, getComputerChoice());
+    div.innerHTML = msg;
 
-    const numberOfGames = 5;
-    let playerScore = 0;
-    let computerScore = 0;
+    if (res == 1){
+        playerScore++;
+    }
 
-    for (let i = 0; i < numberOfGames; i++){
-        let result;
+    if (res == 2){
+        computerScore++;
+    }
+
+    div.innerHTML += `<br />Score: | Player - ${playerScore} | Computer - ${computerScore} |`
+
+    if (playerScore == 5){
+        div.innerHTML += `<br/>You win!! Congratulations`;
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else if (computerScore == 5){
+        div.innerHTML += `<br/>You lose!! Better luck next time.`;
+    }
+    div.innerHTML
+
+
+}
+
+let playerScore = 0;
+let computerScore = 0;
+const div = document.createElement('div');
+
+const btn1 = document.createElement('button');
+btn1.textContent = 'Rock';
+btn1.addEventListener('click', () => ProcessRound(btn1.textContent) );
+
+const btn2 = document.createElement('button');
+btn2.textContent = 'Paper';
+btn2.addEventListener('click', () => ProcessRound(btn2.textContent));
+
+const btn3 = document.createElement('button');
+btn3.textContent = 'Scissors';-
+btn3.addEventListener('click', () => ProcessRound(btn3.textContent));
+
+const root = document.querySelector("body");
+root.appendChild(btn1);
+root.appendChild(btn2);
+root.appendChild(btn3);
+root.appendChild(div);
+
+// function game() {
+
+//     const numberOfGames = 5;
+//     let playerScore = 0;
+//     let computerScore = 0;
+
+//     for (let i = 0; i < numberOfGames; i++){
+//         let result;
         
-        while(true){
-            result = playRound(prompt(), getComputerChoice());
+//         while(true){
+//             result = playRound(prompt(), getComputerChoice());
 
-            if (result == 1){
-                playerScore++;
-                break;
-            }
-            else if (result == 2) {
-                computerScore++;
-                break;
-            } else if (result == 0) {
-                break;
-            }
+//             if (result == 1){
+//                 playerScore++;
+//                 break;
+//             }
+//             else if (result == 2) {
+//                 computerScore++;
+//                 break;
+//             } else if (result == 0) {
+//                 break;
+//             }
 
-        }
+//         }
 
-        console.log(Math.max(playerScore, computerScore), Math.min(playerScore, computerScore), numberOfGames - i)
-        //check if losing player can win with remaining games. End early if not. 
-        if (Math.max(playerScore, computerScore) - Math.min(playerScore, computerScore) > numberOfGames - i)
-            break;
-    }
+//         console.log(Math.max(playerScore, computerScore), Math.min(playerScore, computerScore), numberOfGames - i)
+//         //check if losing player can win with remaining games. End early if not. 
+//         if (Math.max(playerScore, computerScore) - Math.min(playerScore, computerScore) > numberOfGames - i)
+//             break;
+//     }
 
-    if (playerScore > computerScore)
-        alert(`Player wins! ${playerScore} to ${computerScore}`);
-    else 
-        alert(`Computer wins! ${computerScore} to ${playerScore}`);
-}
+//     if (playerScore > computerScore)
+//         alert(`Player wins! ${playerScore} to ${computerScore}`);
+//     else 
+//         alert(`Computer wins! ${computerScore} to ${playerScore}`);
+// }
 
